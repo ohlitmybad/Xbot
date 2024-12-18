61539230187bf7d157e44f329e981d36f5693333
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2024-12-18 13:45:05 +0100

    Update test_twi.py

--- test_twi.py
+++ test_twi.py
@@ -72,7 +72,7 @@ class TestUntitled:
         }
 
         urls = list(urls_and_metrics.keys())
-        weights2 = [0, 0, 0, 0, 0, 0, 1]  # 0.05, 0.17, 0.06, 0.26, 0.21, 0.11, 0.14
+        weights2 = [0.05, 0.17, 0.06, 0.26, 0.21, 0.11, 0.14]  
 
         # Select a URL based on weights
         selected_url = random.choices(urls, weights=weights2, k=1)[0]                
